81355f766179e1bdb35c289e094d2ff32c0efa59
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2023-01-09 16:26:09 -0700

    Add project files.

---
+++ StringExercises/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StringExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
