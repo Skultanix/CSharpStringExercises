d827ddc6161441f10f638b7ef7f7b0968fa7d9b3
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2023-01-09 16:36:27 -0700

    Exercise one completed.

--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            static void ExerciseOne()
+            {
+                Console.WriteLine("Please enter a sequence of numbers separated by a hyphen (-).");
+                var input = Console.ReadLine();
+                string[] splitInput = input.Split("-");
+                List<int> intList = new List<int>();
+
+
+                foreach (string s in splitInput)
+                {
+                    intList.Add(int.Parse(s));
+                }
+
+                var isConsecutive = true;
+                int sequenceDist = 0;
+
+                for (int i = 0; i < intList.Count - 1; i++)
+                {
+                    if (sequenceDist == 0)
+                    {
+                        sequenceDist = intList[i + 1] - intList[i];
+                    }
+                    else
+                    {
+                        if ((intList[i + 1] - intList[i]) != sequenceDist)
+                        {
+                            isConsecutive = false;
+                            break;
+                        }
+                    }
+                }
+                if (isConsecutive == true)
+                {
+                    Console.WriteLine("Numbers are consecutive.");
+                }
+                else
+                {
+                    Console.WriteLine("Numbers are not consecutive");
+                }
+            }
+            ExerciseOne();
+        }  
     }
 }
